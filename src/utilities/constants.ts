6a32ec2e5e22a384f14cd68d4af61f14bf59d387
Zhong-yuen Lee <zl21g13@soton.ac.uk>
2018-03-20 20:40:28 +0000

    Add constants.ts ------ Added this file to contain any constants that'll be used throughout this dashboard. In fact the files from two commits ago already are using this so you know

---
+++ src/utilities/constants.ts
@@ -0,0 +1,10 @@
+/**
+ * This module defines constants for this dashboard. Nothin' much else to say really.
+ */
+export default {
+  /**
+   * The names for each log level, used in RosoutLog and RosoutTable. Each name
+   * should hopefully be pretty self-explanatory.
+   */
+  LOG_LEVELS: ['Debug', 'Info', 'Warn', 'Error', 'Fatal']
+};
