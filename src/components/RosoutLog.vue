b2ec4683b6e52759f5b5ec9f8c0e75734829b65b
Shep <emailmeloh@gmail.com>
2018-04-03 02:11:04 +0100

    Use class method for label styling ------ Using a class from a method for the labels as I need to apply extra styles for when we've selected a particular log level aka clicked on a particular label.

--- src/components/RosoutLog.vue
+++ src/components/RosoutLog.vue
@@ -5,9 +5,9 @@
     <!-- <button class="btn btn-info" @click="togglePause">{{pauseLoggingText}}</button> -->
     <button v-if="D" @click="sendMessage()">DEBUG: Send a message</button>
     <button v-if="D" @click="clearMessages()">DEBUG: Clear all messages</button>
-    <ul class="nav nav-tabs" id="nav-labels">
-      <li class="nav-item" v-for="(l, index) of levels" :key="index" :id="`rosout-level-${l.toLowerCase()}`" @click.prevent="activeLevel = (1 << index)">
-        <a class="nav-link" :class="`${l.toLowerCase()}`" href="/">{{ l }}</a>
+    <ul class="nav nav-tabs border-dark" id="nav-labels">
+      <li class="nav-item" v-for="(l, index) of levels" :key="index" :id="`rosout-level-${l.toLowerCase()}`" @click.prevent.stop="activeLevel = (1 << index)">
+        <a class="nav-link" :class="getClassForLabel(index)" href="/">{{ l }}</a>
       </li>
     </ul>
     <rosout-table :display-level="activeLevel" :new-message="newMessage"></rosout-table>
@@ -33,20 +33,42 @@ export default Vue.component('rosout-log', {
       D: D,
       pauseLoggingText: 'Pause output',
       activeLevel: 4,
-      newMessage: {level: 1, msg: 'Test message'},
+      newMessage: {level: 1, msg: 'Test message level 1'},
       levels: Constants.LOG_LEVELS
     };
   },
   methods: {
-    // Debug methods: They're only in use when the D flag is set
+    /**
+     * Applies the appropriate class name for the Rosout labels
+     * @param {number} level The label's log level
+     * @return {[key: string]: boolean} The class object for Vue to render
+     */
+    getClassForLabel(level: number): {[key: string]: boolean} {
+      return {
+        debug: level === 0,
+        info: level === 1,
+        warn: level === 2,
+        error: level === 3,
+        fatal: level === 4,
+        active: Math.log2(this.activeLevel) === level
+      }
+    },
+    /**
+     * Debug method. Send a dummy message to logs.
+     */
     sendMessage() {
+      if (!D) return;
       const number = 1 << ~~(5 * Math.random());
       this.newMessage = {
         level: number,
         msg: `Test message with level: ${number}`
       };
     },
+    /**
+     * Debug method. Clear all messages from logs.
+     */
     clearMessages() {
+      if (!D) return;
       this.newMessage = {
         level: -1,
         msg: 'clear'
@@ -67,6 +89,7 @@ export default Vue.component('rosout-log', {
     .nav-item {
       .nav-link {
         color: white;
+        // Individual colours for each label
         &.debug {
           background: $debug;
         }
@@ -81,6 +104,9 @@ export default Vue.component('rosout-log', {
         }
         &.fatal {
           background: $fatal;
+        }
+        &.active {
+          font-weight: bold;
         }
       }
     }
